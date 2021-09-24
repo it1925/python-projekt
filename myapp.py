fd4d22aa28eace511a8d4e5fe0ba6ec8de808019
Arnold Rimer <pavelsixta2@seznam.cz>
2021-09-24 02:17:24 +0200

    i had to rename io.py to anything else bcs python does not like it othervise

---
+++ myapp.py
@@ -0,0 +1,17 @@
+otazka1 = input("prezident spojených států je?\n1. J. Biden\n2. D. Trump\n3. R. Nixon\n odpověď:")
+if(int(otazka1)==1):
+    print("spravně")
+else:
+    print("špatně")
+
+otazka2 = input("jakou hodnotu má Eulerovo číslo? \n1. 3.14159\n2. 2\n3. 2.71828\n odpověď:")
+if(int(otazka2)==3):
+    print("spravně")
+else:
+    print("špatně")
+
+otazka3 = input(r"přepis tohoto zapisu v pythonu pi=\sum_{n=0}^\infty(-1)^n\frac{4}{2n+1} je roven?" + "\n1. R\[0]\n2. π\n3. 2.71828\n odpověď:")
+if(int(otazka3)==2):
+    print("spravně")
+else:
+    print("špatně")
